f6332a1b923377df8270198273b77a3ea8292afc
zunafif <60926393+zunafif@users.noreply.github.com>
2020-11-30 10:53:28 +0700

    Add files via upload

---
+++ resources/views/data-modul/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.layout')
+@section('content')
+<div class="main">
+    <div class="main-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Edit data modul</h3>
+                        </div>
+                        <div class="panel-body">
+                            <form action="/modul/{{$modul->id}}/update" method="POST" enctype="multipart/form-data">
+                                {{csrf_field()}}
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">nama modul</label>
+                                    <textarea type="text" name="nama" class="form-control" placeholder="nama" id="exampleInputEmail1" aria-describedby="emailHelp">{{$modul->nama}}</textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">id modul</label>
+                                    <textarea type="text" name="id" class="form-control" placeholder="id" id="exampleInputEmail1" aria-describedby="emailHelp">{{$modul->id}}</textarea>
+                                </div>
+                                <div class=" form-group">
+                                    <label for="exampleInputPassword1">Aktivasi</label>
+                                    <select style="width: 100px" name="aktif" class="form-control" id="exampleInputPassword1">
+                                        <option value="1" @if($modul->aktif == 1) selected @endif>aktif</option>
+                                        <option value="0" @if($modul->aktif == 0) selected @endif>non aktif</option>
+                                    </select>
+                                </div>
+                                <div class="modal-footer">
+                                    <a href="/modul" type="button" class="btn btn-secondary" data-dismiss="modal">Back</a>
+                                    <button type="submit" class="btn btn-primary">update</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="col-md-12">
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/data-modul/modul.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.layout')
+@section('content')
+<div class="main">
+    <div class="main-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Modul</h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="col-6">
+                                <button style="margin-top:10px; margin-left:25px;margin-bottom:1px;" type="button" class="btn btn-success float-right" data-toggle="modal" data-target="#exampleModal">
+                                    Tambah Modul
+                                </button>
+
+                            </div>
+                            @if(session('sukses'))
+                            <div class="alert alert-success" role="alert">
+                                {{session('sukses')}}
+                            </div>
+                            @endif
+                            <div class="panel-body">
+                                <table class="table table-striped table-bordered" id="modul">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama Modul</th>
+                                            <th>Status</th>
+                                            <th>Aksi</th>
+                                            <!-- <th>aksi</th> -->
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php $i = 1 ?>
+                                        @foreach($modul as $u)
+
+                                        <tr>
+                                            <td>{{$i++}}</td>
+                                            <td>{{$u->nama}}</td>
+                                            <td>@if($u->aktif == 1)
+                                                <span class="label label-success"> aktif</span>
+                                                @else
+                                                <span class="label label-warning">non aktif</span>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <form action="{{ route('modul.delete', $u->id)}}" class="d-inline">
+
+                                                    <button class="btn btn-danger btn-sm lnr lnr-trash" type="submit"></button>
+                                                    @csrf
+                                                    <a class="btn btn-primary btn-sm  lnr lnr-pencil" href="{{ route('modul.edit', $u->id) }}"></a>
+                                                </form>
+                                            </td>
+
+                                        </tr>
+                                        @endforeach
+
+                                    </tbody>
+                                </table>
+
+                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Menambahkan Data Modul</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form action="modul/create" method="POST">
+                                                    {{csrf_field()}}
+                                                    <div class="form-group">
+                                                        <label for="exampleInputEmail1">Nama Modul</label>
+                                                        <input type="text" name="nama" class="form-control" placeholder="nama praktikum" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+                                                    </div>
+                                                    <div class="form-group">
+
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Back</button>
+                                                            <button type="submit" class="btn btn-primary">Tambah</button>
+                                                        </div>
+                                                </form>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="col-md-12">
+
+    </div>
+</div>
+@endsection
+
+@section('footer')
+<script>
+    $(document).ready(function() {
+        $('#praktikum').DataTable();
+    });
+</script>
+@stop
